db4fed19bd488cf0822ce6a3d90dfe23504122ee
ldanmer <danmer@inetsys.ru>
2019-06-18 17:19:32 +0300

    some editing

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo "----------------- START INITIAL --------------------"
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone https://github.com/socproof/infra.git
 cd infra
 bash install_ruby.sh
 bash install_mongodb.sh
 bash deploy.sh
+echo "----------------- END INITIAL --------------------"
