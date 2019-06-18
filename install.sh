a651c89004a6d3d9ff54fb697b962ea2ef25ebf2
ldanmer <danmer@inetsys.ru>
2019-06-18 16:38:20 +0300

    some editing

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo apt update && 
-sudo apt install git -y
-git clone git@github.com:socproof/infra.git
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+git clone https://github.com/socproof/infra.git
 cd infra
-./install_ruby.sh &&
-./install_mongodb.sh &&
-./deploy.sh
+bash install_ruby.sh
+bash install_mongodb.sh
+bash deploy.sh
